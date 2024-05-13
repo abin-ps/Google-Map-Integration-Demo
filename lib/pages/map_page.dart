2ef80bbf11a10e2542d8b050b444f1991d83ea84
Abin P S <abinps23@gmail.com>
2024-05-13 21:08:44 +0530

    get users current location (live location) feature example

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,5 +1,8 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -9,27 +12,79 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  final Location _locationController = Location();
+
   static const LatLng _pDevaragudda = LatLng(14.669746789074516, 75.57459681455468);
   static const LatLng _pRaneBennur = LatLng(14.669746789074516, 75.57459681455468);
+
+  LatLng? _currentP;
+
+  @override
+  void initState() {
+    super.initState();
+    _getLocationUpdates();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(target: _pDevaragudda, zoom: 13),
-        markers: {
-          Marker(
-            markerId: MarkerId("_currentLocation"),
-            icon: BitmapDescriptor.defaultMarker,
-            position: _pDevaragudda,
-          ),
-          Marker(
-            markerId: MarkerId("_sourceLocation"),
-            icon: BitmapDescriptor.defaultMarker,
-            position: _pRaneBennur,
-          ),
-        },
-      ),
+      body: _currentP == null
+          ? Center(
+              child: Text('Loading..'),
+            )
+          : GoogleMap(
+              initialCameraPosition: CameraPosition(target: _pDevaragudda, zoom: 13),
+              markers: {
+                 Marker(
+                  markerId: MarkerId("_currentLocation"),
+                  icon: BitmapDescriptor.defaultMarker,
+                  position: _currentP!,
+                ),
+                Marker(
+                  markerId: MarkerId("_sourceLocation"),
+                  icon: BitmapDescriptor.defaultMarker,
+                  position: _pDevaragudda,
+                ),
+                Marker(
+                  markerId: MarkerId("_destinationLocation"),
+                  icon: BitmapDescriptor.defaultMarker,
+                  position: _pRaneBennur,
+                ),
+              },
+            ),
     );
+  }
+
+  Future<void> _getLocationUpdates() async {
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    //check for is service available, if available request permission to access the service. else do nothing
+    _serviceEnabled = await _locationController.serviceEnabled();
+    if (_serviceEnabled) {
+      //if requested service granted returns true else false.
+      _serviceEnabled = await _locationController.requestService();
+    } else {
+      //if service not available for us. terminate the flow. by returning.
+      return;
+    }
+    //do we have location permission
+    _permissionGranted = await _locationController.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await _locationController.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        //if permission not granted. then terminate the flow, by returning.
+        return;
+      }
+    }
+
+    //on location change
+    _locationController.onLocationChanged.listen((LocationData currentLocation) {
+      if (currentLocation.latitude != null && currentLocation.longitude != null) {
+        setState(() {
+          _currentP = LatLng(currentLocation.latitude!, currentLocation.longitude!);
+          print(_currentP);
+        });
+      }
+    });
   }
 }
