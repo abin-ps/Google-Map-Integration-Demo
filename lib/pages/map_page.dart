eece1e85b990b98df9fdac23870a291483eb1a32
Abin P S <abinps23@gmail.com>
2024-05-13 20:27:43 +0530

    simple app that show two marks on map added.

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -8,8 +9,27 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  static const LatLng _pDevaragudda = LatLng(14.669746789074516, 75.57459681455468);
+  static const LatLng _pRaneBennur = LatLng(14.669746789074516, 75.57459681455468);
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(target: _pDevaragudda, zoom: 13),
+        markers: {
+          Marker(
+            markerId: MarkerId("_currentLocation"),
+            icon: BitmapDescriptor.defaultMarker,
+            position: _pDevaragudda,
+          ),
+          Marker(
+            markerId: MarkerId("_sourceLocation"),
+            icon: BitmapDescriptor.defaultMarker,
+            position: _pRaneBennur,
+          ),
+        },
+      ),
+    );
   }
 }
